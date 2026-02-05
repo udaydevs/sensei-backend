0b04ad9c082c9447b0d9525980dad185170bd09c
udaydevs <udaysinghno2005@gmail.com>
2026-02-05 13:57:50 +0530

    fixed api key constants

--- app/core/startup.py
+++ app/core/startup.py
@@ -1,11 +1,11 @@
 """The function that are going to run on startup"""
-import os
 from llama_index.llms.google_genai import GoogleGenAI
 from llama_index.core import Settings, VectorStoreIndex
 from llama_index.embeddings.google_genai import GoogleGenAIEmbedding
 from llama_index.core.node_parser import SentenceSplitter
 from llama_index.vector_stores.qdrant import QdrantVectorStore
 from qdrant_client import QdrantClient
+from app.core.config import settings
 
 llm = None
 query_engine = None
@@ -21,13 +21,13 @@ class LLMManager():
 
         self.llm = GoogleGenAI(
             model="gemini-2.5-flash-lite",
-            api_key=os.getenv("GOOGLE_API_KEY")
+            api_key=settings.LLM_API_KEY
         )
 
         Settings.embed_model = GoogleGenAIEmbedding(
             model_name="text-embedding-004",
             embed_batch_size=100,
-            api_key=os.getenv("GOOGLE_API_KEY"),
+            api_key=settings.LLM_API_KEY,
             max_retries=5,
             timeout=40,
         )
@@ -38,13 +38,13 @@ class LLMManager():
         )
 
         client = QdrantClient(
-            host=os.getenv("QDRANT_HOST"),
-            api_key=os.getenv("QDRANT_API_KEY")
+            host=settings.QDRANT_DB_HOST,
+            api_key=settings.QDRANT_DB_API_KEY
         )
 
         vector_store = QdrantVectorStore(
             client=client,
-            collection_name=os.getenv("QDRANT_COLLECTION_NAME"),
+            collection_name=settings.QDRANT_DB_COLLECTION,
             # enable_hybrid=True 
         )
 
